7eb134bdbf556bcad263fd90ed4f4bb885054678
İsmail Reşat Akcan <iresatakcan.bm@gmail.com>
2018-09-11 14:14:40 +0300

    newsItem to rename and include code

---
+++ app/src/NewsItem.js
@@ -0,0 +1,15 @@
+import React,{Component} from 'react';
+
+
+class NewsItem extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello React!</h1>
+                <p>Lorem ipsum</p>
+            </div>
+        )
+    }
+}
+
+export default NewsItem;
